63f50fa1f06d570af230ebe365aa683bb62dbf4a
razan kurouni <razankr07@gmail.com>
2024-09-11 06:03:19 +0300

    initial commit

--- src/components/slider2/dataSlides.jsx
+++ src/components/slider2/dataSlides.jsx
@@ -14,19 +14,19 @@ const dataSlides = [
     },
     {
         image: image2,
-        title: 'At vero eos et accusamus',
-        text: `tempore, cum soluta nobis est eligendi 
-        optio cumque nihil impedit quo minus id quod maxime placeat
-        facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus 
-        autem quibusdam et aut officiis debitis aut.`
+        title: 'Welcome to Medicio',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat.`
     },
     {
         image: image3,
-        title: 'Temporibus autem quibusdam',
-        text: `Beatae vitae dicta sunt explicabo. Nemo enim ipsam 
-        voluptatem quia voluptas sit aspernatur aut odit aut fugit,
-        sed quia consequuntur magni dolores eos qui ratione voluptatem
-        sequi nesciunt omnis iste natus error sit voluptatem accusantium.`
+        title: 'Welcome to Medicio',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat.`
     }
 ];
 
